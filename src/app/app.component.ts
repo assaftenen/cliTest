443b75619b856e871c8a30a3c56f6f4b047359f1
Assaf <assaf.tenen@gmail.com>
2016-10-14 10:42:21 +0300

    showing some data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Hero} from './shared/hero'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title:string;
+  myHero:string;
+  heroes: any;
+
+  constructor(){
+this.title = 'Tour of Hero',
+this.heroes = [
+   new Hero(1, 'Windstorm'),
+  new Hero(13, 'Bombasto'),
+  new Hero(15, 'Magneta'),
+  new Hero(20, 'Tornado')],
+this.myHero = this.heroes[0]
+
+
+  }
+
 }
+//
