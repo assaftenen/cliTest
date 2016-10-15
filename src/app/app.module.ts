77ddcfff1184fd5f2957f904f0ec358c0e75187e
Assaf <assaf.tenen@gmail.com>
2016-10-16 01:03:40 +0300

    little tour

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import {DashboardComponent} from './dashboard/dashboard.component';
 import { AppComponent } from './app.component';
 import { ClickMeComponent } from './shared/click-me/click-me.component';
 import { KeyupComponent } from './shared/keyup/keyup.component';
+import { LittleTourComponent } from './shared/little-tour/little-tour.component';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { KeyupComponent } from './shared/keyup/keyup.component';
     AppComponent,
     DashboardComponent,
     ClickMeComponent,
-    KeyupComponent
+    KeyupComponent,
+    LittleTourComponent
 
   ],
   imports: [ 
---
+++ src/app/shared/little-tour/little-tour.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { LittleTourComponent } from './little-tour.component';
+
+describe('Component: LittleTour', () => {
+  it('should create an instance', () => {
+    let component = new LittleTourComponent();
+    expect(component).toBeTruthy();
+  });
+});
